f65402e9463b8608b80ba947934ec1e709815a4f
Kit_EA <kit.earf@gmail.com>
2019-01-12 16:04:25 +0500

    remove bootstrap references, app title fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.css";
-import "./index.css";
 import App from "./containers/App.jsx";
+import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
